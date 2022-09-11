9eb1d10eeb121127cd96430c8e96ebc461ad3508
Nakyonyimaj <majorinekayondo@gmail.com>
2022-09-11 13:22:10 +0300

    Step1-create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>
+        List of items
+      </main>
+    </div>
+  );
+}
+
+export default App;
