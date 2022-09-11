2fa4b9de1d1d802888b80cfad620fc43c8797d04
Nakyonyimaj <majorinekayondo@gmail.com>
2022-09-11 15:32:41 +0300

    Step2 - Displaying products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./Data";
 
 function App() {
   return (
@@ -6,10 +7,27 @@ function App() {
         <a href="/">Amazona</a>
       </header>
       <main>
-        List of items
+        <h1>Featured Products</h1>
+        <div className="products">
+          {
+            data.products.map(product => (
+              <div key={product.slug} className="product">
+                <a href={`/product/${product.slug}`}>
+                  <img src ={product.image} alt={product.name}/>
+                  <div className="product-info">
+                    <p>{product.name}</p>
+                    <p>${product.price}</p>
+                  </div>
+                </a>
+                <button>Add to cart</button>
+              </div>
+            ))
+          }
+
+        </div>
       </main>
-    </div>
+    </div>    
   );
-}
+};
 
 export default App;
