2a7e7e74fdd69379e25d8675a8c426668b09e3fa
Nakyonyimaj <majorinekayondo@gmail.com>
2022-09-14 14:58:06 +0300

    fetching data from mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,29 @@
-import express from 'express'
-import data from './Data.js';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
+import express from "express";
+import data from "./Data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI).then(() =>{
-    console.log('connected to mongodb');
-
-})
-.catch((err)=>{
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
     console.log(err.message);
-
-});
+  });
 
 const app = express();
-app.get('/api/products' , (req , res) => {
-    res.send(data.products)
-});
 
-app.get('/api/products/slug/:slug' , (req , res) => {
-    const product = data.products.find((x) => x.slug === req.params.slug);
-    if(product) {
-        res.send(product);
-    }else{
-        res.status(404).send({message: 'Product Not Found'})
-    }
-});
+app.use('/api/seed', seedRouter);
 
-app.get('/api/products/:id' , (req , res) => {
-    const product = data.products.find((x) => x._id === req.params.id);
-    if(product) {
-        res.send(product);
-    }else{
-        res.status(404).send({message: 'Product Not Found'})
-    }
-});
+app.use('/api/products',productRouter);
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`server at http://localhost:${port}`);
+  console.log(`server at http://localhost:${port}`);
 });
